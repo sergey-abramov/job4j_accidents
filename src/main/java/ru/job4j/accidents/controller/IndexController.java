7764ac9b433b0357ace8ccf8f567c31b6aed91d7
beloc <belochuvach@gmail.com>
2023-07-10 13:59:46 +0300

    Загрузка данных в вид

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -9,6 +9,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
