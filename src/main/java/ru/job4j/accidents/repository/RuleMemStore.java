ab21dcffc5ac4d74fbabdcd425792e1b126e79a9
beloc <belochuvach@gmail.com>
2023-07-14 12:02:14 +0300

    Form с агрегационными объектами [#305523 #429185]

---
+++ src/main/java/ru/job4j/accidents/repository/RuleMemStore.java
@@ -0,0 +1,25 @@
+package ru.job4j.accidents.repository;
+
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.model.Rule;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class RuleMemStore {
+
+    private final Map<Integer, Rule> rules = Map.of(
+            1, new Rule(1, "Статья. 1"),
+            2, new Rule(2, "Статья. 2"),
+            3, new Rule(3, "Статья. 3")
+    );
+
+    public Collection<Rule> findAll() {
+        return rules.values();
+    }
+
+    public Set<Rule> findAllByAccident(Accident accident) {
+        return accident.getRules();
+    }
+}
