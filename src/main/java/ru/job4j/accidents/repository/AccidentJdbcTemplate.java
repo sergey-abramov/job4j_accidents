20b61c2b36a20b4515777c7d6344235c03222de6
beloc <belochuvach@gmail.com>
2023-07-15 09:47:14 +0300

    Spring DataSource

--- src/main/java/ru/job4j/accidents/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentJdbcTemplate.java
@@ -2,11 +2,14 @@ package ru.job4j.accidents.repository;
 
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
-import java.util.Collection;
-import java.util.Optional;
+import ru.job4j.accidents.model.AccidentType;
+import ru.job4j.accidents.model.Rule;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class AccidentJdbcTemplate {
@@ -15,7 +18,34 @@ public class AccidentJdbcTemplate {
 
     private final AccidentTypeJdbcStore typeStore;
 
-    private final RuleJdbcStore ruleStore;
+    private final ResultSetExtractor<Map<Integer, Accident>> extractor = (resultSet) -> {
+        Map<Integer, Accident> result = new HashMap<>();
+        while (resultSet.next()) {
+            Accident accident = new Accident(
+                    resultSet.getInt("aId"),
+                    resultSet.getString("aName"),
+                    resultSet.getString("aDescription"),
+                    resultSet.getString("aAddress"),
+                    new AccidentType(
+                            resultSet.getInt("tId"),
+                            resultSet.getString("tName")),
+                    new HashSet<>()
+            );
+            result.putIfAbsent(accident.getId(), accident);
+            result.get(accident.getId()).setRules(Set.of(new Rule(
+                    resultSet.getInt("rId"),
+                    resultSet.getString("rName"))
+            ));
+        }
+        return result;
+    };
+
+    private final RowMapper<Rule> ruleMapper = ((rs, rowNum) -> {
+        Rule rule = new Rule();
+        rule.setId(rs.getInt("id"));
+        rule.setName(rs.getString("name"));
+        return rule;
+    });
 
     private RowMapper<Accident> accidentRowMapper() {
         return (rs, rowNum) -> {
@@ -26,7 +56,6 @@ public class AccidentJdbcTemplate {
             accident.setAddress(rs.getString("address"));
             var type = typeStore.findById(rs.getInt("type_id"));
             type.ifPresent(accident::setType);
-            accident.setRules(ruleStore.findAllByAccident(rs.getInt("id")));
             return accident;
         };
     }
@@ -66,14 +95,27 @@ public class AccidentJdbcTemplate {
     }
 
     public Collection<Accident> findALL() {
-        return jdbc.query("select * from accidents",
-                         accidentRowMapper());
+        return Objects.requireNonNull(jdbc.query("""
+                        select a.id aId, a.name aName, a.description aDescription,
+                        a.address aAddress,t.id tId, t.name tName,
+                        r.id rId, r.name rName from accidents a
+                        join types t on a.type_id = t.id
+                        join rules_accidents ra on a.id = ra.accident_id
+                        join rules r on r.id = ra.rule_id""",
+                extractor)).values();
     }
 
     public Optional<Accident> findById(int id) {
         var accident = jdbc
                 .queryForObject("select * from accidents where id = ?",
                             accidentRowMapper(), id);
+        if (accident != null) {
+            accident.setRules(jdbc.query("""
+                    select r.id, name from rules r
+                    join rules_accidents ra on r.id = ra.rule_id
+                    where accident_id = ?
+                    """, ruleMapper, id).stream().collect(Collectors.toUnmodifiableSet()));
+        }
         return Optional.ofNullable(accident);
     }
 }
--- src/main/java/ru/job4j/accidents/repository/RuleJdbcStore.java
+++ src/main/java/ru/job4j/accidents/repository/RuleJdbcStore.java
@@ -3,12 +3,9 @@ package ru.job4j.accidents.repository;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Rule;
 
 import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class RuleJdbcStore {
@@ -21,14 +18,6 @@ public class RuleJdbcStore {
        rule.setName(rs.getString("name"));
        return rule;
     });
-
-    public Set<Rule> findAllByAccident(int id) {
-        return jdbc.query("""
-                select r.id, name from rules r
-                join rules_accidents ra on r.id = ra.rule_id
-                where accident_id = ?
-                """, mapper, id).stream().collect(Collectors.toUnmodifiableSet());
-    }
 
     public Collection<Rule> findAll() {
         return jdbc.query("select * from rules", mapper);
