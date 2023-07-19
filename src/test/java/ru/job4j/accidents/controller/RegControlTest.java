e4b474ae397f624c9990a224446cb2d7d63581c0
beloc <belochuvach@gmail.com>
2023-07-19 11:53:06 +0300

    Spring test

--- src/test/java/ru/job4j/accidents/controller/RegControlTest.java
+++ src/test/java/ru/job4j/accidents/controller/RegControlTest.java
@@ -8,7 +8,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
